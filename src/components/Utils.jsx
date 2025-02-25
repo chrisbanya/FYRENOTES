450dfb3e1fdd1accb700e0ad10e13d62350b54bf
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-25 01:09:30 +0100

    added credentials styles

--- src/components/Utils.jsx
+++ src/components/Utils.jsx
@@ -23,8 +23,22 @@ export const LayoutClasses = {
     backgroundColor: "#f9f9f9",
     width: "100%",
     padding: (theme) => theme.spacing(3),
+    marginTop: "60px",
   },
   title: {
     padding: (theme) => theme.spacing(2),
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+  },
+  toolbar: (theme) => theme.mixins.toolbar,
+};
+export const CredentialStyle = {
+  center: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
   },
 };
