92e83a8dbe8dffe819190a9e9bbb09cd467ecc5b
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-27 00:02:09 +0100

    added retrival and deletion of data from db

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,42 +1,59 @@
 import { useState, useEffect } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
-
 import Grid from "@mui/material/Grid2";
 import { Container } from "@mui/material";
+import { auth } from "../firebase/config";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+  doc,
+  deleteDoc
+} from "firebase/firestore";
+import { db } from "../firebase/config";
+
 import NoteCard from "../components/NoteCard";
-const URL = "http://localhost:8000/notes";
 
 export const Notes = () => {
   const [notes, setNotes] = useState([]);
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const user = auth?.currentUser;
 
   useEffect(() => {
-    async function fetchNotes() {
-      try {
-        const res = await fetch(URL);
-        if (!res.ok) {
-          throw new Error("failed to fetch notes");
-        }
-        const data = await res.json();
-        setNotes(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchNotes();
-  }, [notes]);
+    if (!user) return;
+    setIsLoading(true)
+
+    const dbQuery = query(
+      collection(db, "notes"),
+      where("userId", "==", user.uid),
+      orderBy("createdAt", "desc")
+    );
+    const unsubscribe = onSnapshot(dbQuery, (snapshot) => {
+      const notesArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNotes(notesArray);
+      setIsLoading(false)
+    });
+    return () => unsubscribe();
+  }, [user]);
 
   const handleDelete = async (id) => {
-    await fetch("http://localhost:8000/notes/" + id, {
-      method: "DELETE",
-    });
-  };
-  if (error) {
-    return <p style={{ color: "red" }}>{error}</p>;
+        if (!window.confirm("Are you sure you want to delete this note?"))
+          return;
+    try {
+      await deleteDoc(doc(db, "notes", id))
+      setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id))
+      console.log("deleted success")
+    } catch(error) {
+      console.error(error)
+    }
+  
   }
 
   return (
@@ -53,6 +70,7 @@ export const Notes = () => {
         ) : (
           <p>No notes found</p>
         )}
+      
       </Grid>
     </Container>
   );
