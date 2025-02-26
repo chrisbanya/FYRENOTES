ca6a831e9d9c9f3be8ebbadd52013354a0890d23
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-27 00:05:01 +0100

    added firebase auth

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -6,83 +6,114 @@ import {
   Container,
   Paper,
 } from "@mui/material";
-import { LogInStyle } from "../components/Utils";
+import CircularProgress from "@mui/material/CircularProgress";
+import { CredentialStyle } from "../components/Utils";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/config";
 
 const LogIn = () => {
-    const [credentials, setCredentials] = useState({
-      username: "",
-      password: "",
-    });
-    const [usernameError, setUsernameError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-    function handleChange(e){
-        setCredentials({...credentials, [e.target.name]: e.target.value})
-        console.log(credentials)
+  function handleChange(e) {
+    setError("");
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setEmailError(false);
+    setPasswordError(false);
+    if (credentials.email == "") {
+      setEmailError(true);
     }
-
-    function handleSubmit(e){
-           e.preventDefault();
-           setUsernameError(false);
-           setPasswordError(false);
-           if (credentials.username == "") {
-             setUsernameError(true);
-           }
-           if (credentials.password == "") {
-             setPasswordError(true);
-           }
-           if (credentials.username !== "" && credentials.password !== ""){
-            console.log(credentials)
-           }
-
+    if (credentials.password == "") {
+      setPasswordError(true);
     }
+    if (credentials.email !== "" && credentials.password !== "") {
+      setIsLoading(true);
+      signInWithEmailAndPassword(auth, credentials.email, credentials.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          setIsLoading(false);
+          navigate("/create");
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsLoading(false);
+        });
+    }
+  }
   return (
     <Container component="main" maxWidth="xs">
-      <Box sx={LogInStyle.center}>
+      <Box sx={CredentialStyle.center}>
+        {isLoading && (
+          <Box xs={12}>
+            <CircularProgress color="secondary" size="5rem" />
+          </Box>
+        )}
         <Paper elevation={3} sx={{ padding: 3 }}>
           <Typography component="h1" variant="h5" align="center">
             Login
           </Typography>
           <Box sx={{ mt: 1 }}>
             <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-              error={usernameError}
-            onChange={handleChange}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              error={passwordError}
-            onChange={handleChange}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Login
-            </Button>
-            <Typography component="h3" variant="subtitle1" align="center">
-              Don&apos;t have an account?{" "}
-            </Typography>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={credentials.email}
+                onChange={handleChange}
+                error={emailError}
+                helperText={emailError ? "Email is required" : ""}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={passwordError}
+                onChange={handleChange}
+                helperText={
+                  passwordError ? "Passwords must be atleast 6 characters" : ""
+                }
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </Button>
+              {error && (
+                <Typography variant="subtitle1" align="center" color="error">
+                  {error}
+                </Typography>
+              )}
+              <Typography component="h3" variant="subtitle1" align="center">
+                Don&apos;t have an account? <Link to="/Register">Register</Link>
+              </Typography>
             </form>
           </Box>
         </Paper>
