d0a4204e1a7641b78dc9baa80fac6ec84b78b6e1
Adabanya Christian <adabanyachristian21@gmail.com>
2025-03-01 11:52:35 +0100

    resusable loading spinner

---
+++ src/components/CircularProgressComp.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+
+
+export default function CircularProgressComp() {
+  return (
+    <Box
+      xs={12}
+      sx={{
+        position: "fixed",
+        inset: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "rgba(17, 24, 39, 0.5)",
+        zIndex: 60,
+      }}
+    >
+      <CircularProgress color="secondary" size="3rem" />
+    </Box>
+  );
+}
